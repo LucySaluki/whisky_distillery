795ec504e4fcebd942d9502e2b315b3e10884b4f
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-10-08 09:22:34 +0100

    MVP finished

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -0,0 +1,42 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class WhiskyController {
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @Autowired
+    DistilleryRepository distilleryRepository;
+
+    @GetMapping(value="/whiskies")
+    public ResponseEntity<List<Whisky>> getAllWhiskies(
+            @RequestParam(name="year", required = false) Integer year,
+            @RequestParam(name="age", required = false) Integer age,
+            @RequestParam(name="distillery", required = false) String distillery,
+            @RequestParam(name="region", required = false) String region){
+
+        if (year!= null) {
+            return new ResponseEntity<>(whiskyRepository.findWhiskyByYear(year), HttpStatus.OK);
+        } else if (distillery!=null && age!= null){
+            Distillery distilleryObj = distilleryRepository.findDistilleryByName(distillery);
+            return new ResponseEntity<>(whiskyRepository.findWhiskyByAgeAndDistilleryId(age,distilleryObj.getId()), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+        }
+    }
+
+}
---
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -0,0 +1,49 @@
+package com.codeclan.example.WhiskyTracker;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhiskyTrackerApplicationTests {
+
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
+	@Autowired
+	DistilleryRepository distilleryRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void canFindWhiskeyByYear(){
+		List<Whisky> found = whiskyRepository.findWhiskyByYear(2018);
+		assertEquals(6, found.size());
+	}
+
+	@Test
+	public void canFindDistilleryByRegion(){
+		List<Distillery> found = distilleryRepository.findDistilleryByRegion("Highland");
+		assertEquals(3, found.size());
+	}
+
+	@Test
+	public void canFindWhiskyByAgeByDistillery(){
+		Distillery distillery = distilleryRepository.findDistilleryByName("Highland Park");
+		List<Whisky> found = whiskyRepository.findWhiskyByAgeAndDistilleryId(12,distillery.getId());
+		assertEquals("Viking Honour",found.get(0).getName());
+	}
+}
